b2507ed52ac9d6c82e38d05a55643c9a09903137
Gail Parazo <202310763@gordoncollege.edu.ph>
2025-01-13 23:25:24 +0800

    addNotebtn updated using CardLayout

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 package src;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Main {
 
@@ -12,17 +14,17 @@ public class Main {
     window.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
     window.setSize(800,600);
 
+// Create the main panel
+CardLayout cardLayout = new CardLayout();
+JPanel mainPanel = new JPanel(cardLayout);
+ 
+
 //topbar panel for menus/the bar is like the top sectioning above like a border "BorderLayput aligns components to North, south, east, west and center"
     JPanel topbar = new JPanel (new BorderLayout());
     topbar.setBackground(Color.decode("#89f594"));
 
-//CardLayout Panel for tab switching
-    JPanel cardPanel = new JPanel (new CardLayout());
 
-// Create the main panel
-    JPanel mainPanel = new JPanel(new BorderLayout());
 
-    
 
 //adding menu image icon to the top bar
     ImageIcon menu = new ImageIcon ("images/menu.png");
@@ -34,7 +36,7 @@ public class Main {
 //This RightPanel("RightPanel is for the right side og the top bar"), then we will put our light and dark mode components there
    JPanel rightPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT, 1, 0));
 
-//adding darkmode icon to top bar
+//adding darkmode icon to top bar using RIGHT PANEL 
      ImageIcon darkmode = new ImageIcon ("images/dark.gif");
         JButton darkbtn = new JButton (darkmode);
         darkbtn.setPreferredSize (new Dimension (30,30));
@@ -55,7 +57,7 @@ public class Main {
     notePanel.setBackground(Color.decode("#fc4e7d"));
     notePanel.setBorder(BorderFactory.createEmptyBorder(0,40,0,0));
     ImageIcon addNote = new ImageIcon ("images/pencil.png");
-         
+          
         //JButton addNote
         JButton addNotebutton = new JButton (addNote);
         addNotebutton.setPreferredSize (new Dimension (50,50));
@@ -67,12 +69,24 @@ public class Main {
         notePanel.add (addNotebutton);
         notePanel.add (addnoteLabel);
 
+     
+
+        //CardLayout Panel for tab switching
+    JPanel newNote = new JPanel ();
+    newNote.setBackground (Color.decode("#1f0924"));
+
+    //main panel 
+    mainPanel.add(notePanel, "MainDashboard");
+    mainPanel.add(newNote,"newNote");
+
     //actionListener addnote
-        addNotebutton.addActionListener(e -> {
-            CardLayout cl =(CardLayout)(cardPanel.getLayout());
-            cl.show(cardPanel,"AddNotepanel");
+        addNotebutton.addActionListener (new ActionListener() {
+            @Override
+            public void actionPerformed (ActionEvent e) {
+                
+                cardLayout.show (mainPanel, "newNote");
+            }
         });
-    
         
 
 
@@ -80,8 +94,9 @@ public class Main {
     //window.getContentPane().setBackground(Color.decode("#f77799")); 
     //getContentPane method is part of java swing librrary so you cannot rename it!
     window.add (topbar,BorderLayout.NORTH);
-    window.add(notePanel,BorderLayout.CENTER);
-    window.setVisible(true);
+    //window.add(notePanel,BorderLayout.CENTER);
+     window.add (mainPanel);
+    window.setVisible(true); 
 
    } 
 }
